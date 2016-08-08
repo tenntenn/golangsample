1eb3943a70d27cbfbc142c38b13b84330566dba8
makoto tano <makoto.tano@gmail.com>
2016-08-08 21:40:26 +0900

    change books

--- server.go
+++ server.go
@@ -15,24 +15,36 @@ func main() {
 	e := echo.New() // HTTPサーバーのハンドリング、Contextの生成、パラメータ処理、ルーティングの処理 etc
 
 	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
+		templates: template.Must(template.ParseGlob("public/views/*/*.html")),
 	}
 	e.SetRenderer(t)
 
 	e.Use(middleware.Logger()) // $ go get github.com/dgrijalva/jwt-go
 
-	e.GET("/", showHello)
-	e.Run(standard.New(":1323"))
+	e.GET("/books/new", add)
+	e.GET("/books/:id", show)
+
+	e.Run(standard.New(":1234"))
 }
 
-func showHello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", map[string]string{
+func add(c echo.Context) error {
+	return c.Render(http.StatusOK, "books/add", map[string]string{
 		"world":  "World",
 		"myName": "John",
 	})
 }
 
-// Template difinition
+func show(c echo.Context) error {
+	id := c.Param("id")
+	_ = id
+
+	return c.Render(http.StatusOK, "books/show", map[string]string{
+		"world":  "World",
+		"myName": "John",
+	})
+}
+
+// Template definition
 type Template struct {
 	templates *template.Template
 }
