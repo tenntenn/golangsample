2007de15912f9906f630ce66b977863edc3a95d6
makoto tano <makoto.tano@gmail.com>
2016-08-08 17:41:10 +0900

    sample on echo tutorial

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+)
+
+// this is a sample code on https://github.com/labstack/echo
+func main() {
+	e := echo.New() // HTTPサーバーのハンドリング、Contextの生成、パラメータ処理、ルーティングの処理 etc
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Run(standard.New(":1323"))
+}
